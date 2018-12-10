5248dbee001d23cd1ed0e14d35f9468590d207da
Alex <alex.jacquot.dev@gmail.com>
2018-12-10 10:47:14 +0100

    Major revamps.Some stuff needs to be reworked still.

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    console.log("Loaded page, hiding loader");
+    document.getElementById("loader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+});
